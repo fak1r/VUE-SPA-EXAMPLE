fbdf78da665522cf62418041d3f16b1b941af8b6
fak1r <fak1r@yandex.ru>
2023-04-25 19:26:34 +0300

    Checkout

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,13 +1,52 @@
 <template>
-  <h1>Order now</h1>
-  <hr>
-  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas enim architecto odio perspiciatis quos at quisquam, iste aliquid unde, repellendus sequi quis beatae tempora vero dolores minus voluptatibus eius nam?</p>
+  <div v-if="!buy">
+    <h1>Order now</h1>
+    <hr>
+    <table class="table table-striped">
+      <thead class="table-dark">
+      <tr>
+        <td>Title</td>
+        <td>Price</td>
+        <td>Cnt</td>
+        <td>Total</td>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="product in productsInCart" :key="product.id">
+        <td>{{ product.title }}</td>
+        <td>{{ product.price }}</td>
+        <td>{{ product.cnt }}</td>
+        <td>{{ product.price * product.cnt }}</td>
+      </tr>
+      </tbody>
+    </table>
+    <div align="right">
+      <h2>Total: {{ total }}</h2>
+      <button type="button" class="btn btn-primary" @click="order">Buy</button>
+    </div>
+  </div>
+  <div v-else>
+    <h1>Thanks for buying</h1>
+  </div>
   <router-view></router-view>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
-  name: "Checkout"
+  name: "Checkout",
+  data: () => ({
+    buy: false
+  }),
+  computed:{
+    ...mapGetters('cart', ['productsInCart', 'total']),
+  },
+  methods:{
+    order(){
+      return this.buy = true;
+    }
+  }
 }
 </script>
 
